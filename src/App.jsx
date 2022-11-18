eee9d83f7202a06e06406517f43f6209c1238619
Alan Torquatro de Matos <mr.alanmatos@hotmail.com>
2022-11-18 15:03:00 -0500

    clean up

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from './components/login/Login.jsx';
+
+
+
+const App = () => {
+  return (
+    <>
+    <Login></Login>
+    </>
+  )
+}
+
+export default App
